f6e7e9fc2f5495c2022441f0aada3aabb32b05d1
Samuel Gref <samgref@gmail.com>
2021-12-31 17:23:01 -0500

    Header

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clintID:
+          "1061492136540-ssfpgghi16l77cs2j5mdf45ueluh6bf3.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
